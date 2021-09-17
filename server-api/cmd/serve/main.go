5708fc2e6cb6f5b13b68bafa9efdfab422f535d7
Steven Jimenez <stevenmjimenez@gmail.com>
2021-09-16 23:34:16 -0400

    Created the swagger API documentation

--- server-api/cmd/serve/main.go
+++ server-api/cmd/serve/main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"server-api/internal/hello_world"
+	"encoding/json"
+	"log"
+	"net/http"
 )
 
+type Router struct{}
+
+func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	log.Printf("HTTP %s %s", req.Method, req.URL.Path)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode("Hello, world"); err != nil {
+		log.Printf("ERROR: Could not encode response: %s\n", err)
+	}
+}
+
 func main() {
-	fmt.Println(hello_world.GetHelloWorld())
+	listenOn := ":3000"
+
+	r := Router{}
+
+	log.Fatal(http.ListenAndServe(listenOn, &r))
 }
